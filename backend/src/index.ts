bbaf12500c1de94942f721bf34949797e80859a8
Aryan Mane <aryanmane7916@gmail.com>
2024-05-29 15:10:04 +0530

    completed db migration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+
+const prisma = new PrismaClient({
+  datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+  return c.text('signup')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('post blog')
+})
+
+app.put('/api/v1/bolg', (c) => {
+  return c.text('edit blog')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('get blog')
 })
 
 export default app
